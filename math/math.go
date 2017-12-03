3caf45c56af4a58593dde42712d83d6fd5632ec6
Andrew Wong <andrew.wong@pelotoncycle.com>
2017-12-03 10:24:07 -0500

    vector math

--- math/math.go
+++ math/math.go
@@ -2,6 +2,36 @@ package math
 
 import "fmt"
 
+type Point struct {
+    x, y int
+}
+type Vec struct {
+    x, y int
+}
+
+type Triangle = [3]Point
+
+func DotProduct(a, b Vec) int {
+    return (a.x * b.x) + (a.y * b.y)
+}
+
+func Normal(a Vec) Vec {
+    return Vec{-a.y, a.x}
+}
+
+func Inside(a, b, c Point) bool {
+    aToB := Vec{b.x - a.x, b.y - a.y}
+    aToC := Vec{c.x - a.x, c.y - a.y}
+
+    return DotProduct(Normal(aToB), aToC) >= 0
+}
+
+func InsideTriangle(t Triangle, c Point) bool {
+    return Inside(t[0], t[1], c) &&
+           Inside(t[1], t[2], c) &&
+           Inside(t[2], t[0], c)
+}
+
 func Test() {
     fmt.Printf("math  t\n")
 }
--- math/math_test.go
+++ math/math_test.go
@@ -2,6 +2,44 @@ package math
 
 import "testing"
 
-func TestTest(t *testing.T) {
+func TestDotProduct(t *testing.T) {
+    v1 := Vec{0,1}
+    v2 := Vec{1,0}
+    if DotProduct(v1, v2) != 0 {
+        t.Errorf("bad dot product")
+    }
+    if DotProduct(Vec{0,3}, Vec{1,2}) != 6 {
+        t.Errorf("bad dot product")
+    }
+}
 
+func TestInside(t *testing.T) {
+    p1 := Point{0,0}
+    p2 := Point{10,0}
+
+    if !Inside(p1, p2, Point{1, 2}) {
+        t.Errorf("bad inside")
+    }
+    if !Inside(p1, p2, Point{-5, 0}) {
+        t.Errorf("bad inside")
+    }
+    if Inside(p1, p2, Point{5, -1}) {
+        t.Errorf("bad inside")
+    }
+}
+
+func TestInsideTriangle(t *testing.T) {
+    tri := Triangle{Point{0, 0}, Point{10, 0}, Point{10, 10}}
+    if !InsideTriangle(tri, Point{5, 1}) {
+        t.Errorf("bad inside triangle")
+    }
+    if !InsideTriangle(tri, Point{5, 0}) {
+        t.Errorf("bad inside triangle")
+    }
+    if InsideTriangle(tri, Point{5, -1}) {
+        t.Errorf("bad inside triangle")
+    }
+    if InsideTriangle(tri, Point{5, 10}) {
+        t.Errorf("bad inside triangle")
+    }
 }
